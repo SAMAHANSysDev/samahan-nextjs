44734b3233ff1dd5e5fdb227a8478c3efa06cc00
sonroyaalmerol <sonroya.almerol@gmail.com>
2020-07-23 01:45:12 +0800

    add placeholder news grids

--- pages/newsfeed/index.js
+++ pages/newsfeed/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { fade, makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Head from 'next/head';
+import WP from '../../utils/wordpress';
+import Item from '../../components/home/newsfeed-item';
 
 const useStyles = makeStyles((theme) => ({
   contentContainer: {
@@ -14,7 +17,7 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const Page = () => {
+const Page = ({ posts }) => {
   // Get the data of the current list.
   const classes = useStyles();
 
@@ -30,9 +33,30 @@ const Page = () => {
         <meta property="og:description" content="News and Stories of and for the Student Body" />
       </Head>
       {/* Insert header here! */}
-      Hello World!
+      <div className={classes.contentContainer}>
+        <Grid container direction="row" spacing={3} alignItems="stretch">
+          {posts.map((post) => {
+            // Render one Item component for each one.
+            return <Item key={post.id} item={post} />;
+          })}
+        </Grid>
+      </div>
     </div>
   );
 };
+
+Page.getInitialProps = async (ctx) => {
+  try {
+    const res = await WP.posts();
+    if (res) { 
+      return { posts: res };
+    } else {
+      return { posts: [] };
+    }
+  } catch (err) {
+    console.log(err);
+    return { posts: [] };
+  }
+}
 
 export default Page;
