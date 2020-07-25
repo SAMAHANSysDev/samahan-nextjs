9f9d900cb59f4d0622c40f855acc3933146dcf7c
sonroyaalmerol <sonroya.almerol@gmail.com>
2020-07-25 14:01:06 +0800

    move cdn

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,5 @@
 const frontendURL = 'https://samahan.vercel.app';
 const backendURL = 'https://samahan.addu.edu.ph';
-const cdnURL = 'https://samahan-cdn.snry.xyz';
+const cdnURL = 'https://samahan-cdn.imfast.io';
 
 export { frontendURL, backendURL, cdnURL };
