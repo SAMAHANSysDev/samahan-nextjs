221afe8f516bf3e60a609854da1f9318f84986e9
sonroyaalmerol <sonroya.almerol@gmail.com>
2020-08-08 13:12:51 +0800

    add images to himig-bughaw

--- pages/himig-bughaw.js
+++ pages/himig-bughaw.js
@@ -5,6 +5,8 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
+import { Carousel } from 'react-responsive-carousel';
+
 import { cdnURL } from 'utils/constants';
 
 import dynamic from 'next/dynamic';
@@ -13,6 +15,9 @@ const Banner = dynamic(() => import('components/himig-bughaw/banner'));
 const useStyles = makeStyles((theme) => ({
   spacer: {
     height: 30
+  },
+  spacerDiv: {
+    height: 100
   },
   contentContainer: {
     width: '80%',
@@ -49,11 +54,42 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.secondary.main,
     color: '#000'
   },
+  carouselImage: {
+    width: 'auto',
+    maxHeight: 400
+  },
+  carouselDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'block',
+    },
+  },
+  carouselMobile: {
+    display: 'block',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  }
 }));
+
 
 const Page = ({ events: rawEvents }) => {
   // Get the data of the current list.
   const classes = useStyles();
+
+  const PartnerCarousel = () => {
+    return (
+      <Carousel autoPlay={false} showThumbs={false} showArrows={true} className={classes.carousel}>
+        { [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map((number) => {
+          return (
+            <div>
+              <img key={number} src={`${cdnURL}/himig_${number}.jpg`} className={classes.carouselImage} />
+            </div>
+          );
+        }) }
+      </Carousel>
+    )
+  }
 
   return (
     <div className={classes.rootContainer}>
@@ -72,12 +108,32 @@ const Page = ({ events: rawEvents }) => {
                 All funds generated from the concert will directly go to the Foundation of Balay Pasilungan, Inc.
               </Typography>
               <div className={classes.spacer} />
+            </Grid>
+            <Grid item xs={12} md={5}>
+              <img src={`${cdnURL}/samahan_logo_colored.png`} style={{ width: '45%', marginRight: '5%' }} />
+              <img src={`${cdnURL}/aosf_logo.png`} style={{ width: '45%', marginLeft: '5%' }} />
+            </Grid>
+          </Grid>
+          <div className={classes.spacerDiv} />
+          <Grid container direction="row" spacing={5} alignItems="center">
+            <Grid item xs={12} md={5} className={classes.carouselDesktop}>
+              <PartnerCarousel />
+            </Grid>
+            <Grid item xs={12} md={7}>
               <Typography variant="h2" component="p">About the Partner Community</Typography>
               <div className={classes.spacer} />
               <Typography variant="body1" component="p">
                 The Balay Pasilungan of the Society of Mary-Marist Fathers and Brothers provides a <b>safe place</b> for young street boys who have no other place to run to when in distress. The foundation envisions a community where <b>underprivileged boys</b> will have a productive and wholesome adult lives hoping that they will have <b>better integration in the society</b>. 
               </Typography>
               <div className={classes.spacer} />
+            </Grid>
+            <Grid item xs={12} md={5} className={classes.carouselMobile}>
+              <PartnerCarousel />
+            </Grid>
+          </Grid>
+          <div className={classes.spacerDiv} />
+          <Grid container direction="row" spacing={3} alignItems="center">
+            <Grid item xs={12} md={7}>
               <Typography variant="h2" component="p">Donation Guidelines</Typography>
               <div className={classes.spacer} />
               <Button variant="contained" size="large" color="primary" fullWidth disableElevation onClick={()=> window.open("https://tinyurl.com/HimigBughaw", "_blank")}>
