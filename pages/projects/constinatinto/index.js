493780a59e25396f92658bb850e92029d2a2dda3
sonroyaalmerol <sonroya.almerol@gmail.com>
2020-09-05 00:22:40 +0800

    revise constinatinto page

--- pages/projects/constinatinto/index.js
+++ pages/projects/constinatinto/index.js
@@ -7,9 +7,23 @@ import Grid from '@material-ui/core/Grid';
 
 import { useRouter } from 'next/router';
 
+import { Carousel } from 'react-responsive-carousel';
+
 import Button from '@material-ui/core/Button';
 
 import { cdnURL } from 'utils/constants';
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
 
 const useStyles = makeStyles((theme) => ({
   contentContainer: {
@@ -19,9 +33,88 @@ const useStyles = makeStyles((theme) => ({
   
   rootContainer: {
     width: '100%',
-    marginBottom: 80
+    marginBottom: 80,
   },
+  avp: {
+    width: '80vw',
+    height: '80vw',
+    [theme.breakpoints.up('md')]: {
+      width: '30vw',
+      height: '30vw',
+    }
+  },
+  cardContainer: {
+    width: '72vw',
+    [theme.breakpoints.up('sm')]: {
+      width: '36vw',
+    },
+    [theme.breakpoints.up('md')]: {
+      width: '18vw',
+    }
+  },
+  imageContainer: {
+    height: '72vw',
+    [theme.breakpoints.up('sm')]: {
+      height: '36vw',
+    },
+    [theme.breakpoints.up('md')]: {
+      height: '18vw',
+    }
+  }
 }));
+
+const constiSlides = () => (
+  <Carousel autoPlay={false} showThumbs={true} showArrows={true}>
+    <div>
+      <img src={`${cdnURL}/Constitution_0.jpg`} />
+    </div>
+    <div>
+      <img src={`${cdnURL}/Constitution_1.jpg`} />
+    </div>
+    <div>
+      <img src={`${cdnURL}/Constitution_2.jpg`} />
+    </div>
+  </Carousel>
+)
+
+const functionSlides = () => (
+  <Carousel autoPlay={false} showThumbs={true} showArrows={true}>
+    <div>
+      <img src={`${cdnURL}/Function_0.jpg`} />
+    </div>
+    <div>
+      <img src={`${cdnURL}/Function_1.jpg`} />
+    </div>
+    <div>
+      <img src={`${cdnURL}/Function_2.jpg`} />
+    </div>
+  </Carousel>
+)
+
+const ratifySlides = () => (
+  <Carousel autoPlay={false} showThumbs={true} showArrows={true}>
+    <div>
+      <img src={`${cdnURL}/Ratify_0.jpg`} />
+    </div>
+    <div>
+      <img src={`${cdnURL}/Ratify_1.jpg`} />
+    </div>
+  </Carousel>
+)
+
+const reviseSlides = () => (
+  <Carousel autoPlay={false} showThumbs={true} showArrows={true}>
+    <div>
+      <img src={`${cdnURL}/Revise_0.jpg`} />
+    </div>
+    <div>
+      <img src={`${cdnURL}/Revise_1.jpg`} />
+    </div>
+    <div>
+      <img src={`${cdnURL}/Revise_2.jpg`} />
+    </div>
+  </Carousel>
+)
 
 const Page = () => {
   // Get the data of the current list.
@@ -29,69 +122,154 @@ const Page = () => {
   const classes = useStyles();
   const router = useRouter();
 
+  const [openInfog, setInfog] = React.useState(false);
+  const [slide, setSlide] = React.useState(null);
+
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const handleClickOpen = (x) => {
+    setInfog(true);
+    switch (x) {
+      case 0:
+        setSlide(constiSlides);
+        break;
+      case 1:
+        setSlide(functionSlides);
+        break;
+      case 2:
+        setSlide(reviseSlides);
+        break;
+      case 3:
+        setSlide(ratifySlides);
+    }
+  };
+
+  const handleClose = () => {
+    setInfog(false);
+  };
+
   return (
     <div className={classes.rootContainer}>
 
       <div style={{ height: 100 }} />
 
-      <Grid container direction="row" spacing={3} alignItems="center" className={classes.contentContainer}>
-        <Grid item md={7}>
-          <Typography variant="h1" component="h2" style={{ lineHeight: '0.8em' }}>
-            #ConstiNatinTo
-          </Typography><br />
-          <Typography variant="subtitle1" component="h2" style={{ lineHeight: '1.5em' }}>
-            SAMAHAN Constitutional Reform for AY 2020-2021
-          </Typography>
-          <Typography variant="subtitle1" component="h2" style={{ lineHeight: '1.5em' }}>
-            samahan.addu.edu.ph
-          </Typography>
-        </Grid>
-        <Grid item md>
-          <img src={`${cdnURL}/board-consti.png`} style={{ width: '100%', height: 'auto' }} />
-        </Grid>
-      </Grid>
-
-      <div style={{ height: 100 }}></div>
-
       <div style={{ margin: 'auto', width: '90%' }}>
-        <Grid container direction="row" spacing={6} className={classes.contentContainer}>
-          <Grid item sm={8}>
-            <Typography variant="h4" component="h2">What is the Constitution?</Typography>
-            <Typography variant="body1" component="p" style={{ marginTop: '10px', marginBottom: '10px' }}>In general, the constitution is considered as the highest law of the land. The constitution describes the basic principles of the state which revolves around the separation of powers, checks and balances, people’s participation, political disposition, etc. The constitution also includes the structures and processes of the government wherein it identifies the rudimentary national positions, branches of government, hierarchy of power, etc. It also states and identifies the processes of budget dissemination, law-making, bureaucracy, etc. Lastly, the constitution expresses the fundamental rights of the constituents which cannot be changed by a simple legislative act.</Typography>
-            <Typography variant="body1" component="p" style={{ marginTop: '10px', marginBottom: '10px' }}>In the context of SAMAHAN, the SAMAHAN constitution is the foundation of the rights and welfare of the students. It states the basic principles of the student government in representing the student body of Ateneo de Davao University. It also includes the scope of SAMAHAN itself, from its systems and processes, student government structure, and limitation of powers. The SAMAHAN Constitution protects the autonomy of students through emphasizing its capability of self-governance.</Typography>
-            <Typography variant="body1" component="p" style={{ marginTop: '10px', marginBottom: '10px' }}>To check the revision progress, </Typography>
-            <Button variant="contained" color="primary" onClick={() => router.push('/projects/constinatinto/the-revision-progress')}>Click here</Button>
-            <div style={{ height: 50 }}></div>
-            <Typography variant="h4" component="h2">Functions of the Constitution</Typography>
-            <Typography variant="body1">
-              <ul>
-                <li>The SAMAHAN constitution legitimizes the student government of the university. It enables the SAMAHAN to function as a student government.</li>
-                <li>The SAMAHAN constitution serves as the embodiment of democracy within the institution, which enables the student government to recognize the power of the student population in decision making.</li>
-                <li>It serves as a guide in administering the Ateneo students, through defining the limitation of powers of the students in position, and by stating the detailed system and processes of the student government. Which identifies the constitutionality of each and every action that has been or will be committed.</li>
-                <li>It states and protects the fundamental rights of students as a constituent of the university and SAMAHAN.</li>
-                <li>It declares the capacity of students for self-governance and decision making.</li>
-                <li>It resolves conflict in terms of issues concerning, impeachment, elections, autonomy, and misuse of powers.</li>
-                <li>The SAMAHAN Constitution is the basis for obtaining accountability.</li>
-              </ul>
+
+        <Grid container direction="row" spacing={3} alignItems="center" className={classes.contentContainer}>
+          <Grid item md={7} xs={12}>
+            <Typography variant="h1" component="h2" style={{ lineHeight: '0.8em' }}>
+              Consti Natin To!
+            </Typography><br />
+            <Typography variant="h5" component="h2" style={{ lineHeight: '1.5em' }}>
+              Bagong Consti, Bagong SAMAHAN
+            </Typography>
+            <Typography variant="body1" component="div" style={{ marginTop: '10px', marginBottom: '10px' }}>
+              <p>This academic year, the SAMAHAN Central Board with the leadership of the SAMAHAN Department of Students Rights and Welfare reinforces its solidified effort to finally translate a dream into a reality. </p>
+              <p>After years reform attempts, it is about time that the student body shall come together to pave the way to the genesis of a new SAMAHAN Constitution. A constitution that is responsive to the current setting of Ateneo, a constitution that could withstand the changing of times, and a constitution that puts premium significance to wider student representation and autonomy. </p>
+              <p>Let us all unite and take part of this change. This is our <b>constitution – CONSTI NATIN TO! Bagong Consti, Bagong SAMAHAN.</b></p>
             </Typography>
           </Grid>
-          <Grid item sm>
-            <Typography variant="h4" component="h2" style={{ marginBottom: 10 }}>Contact Personnels</Typography>
-            <Grid container direction="row">
-              <Grid item sm style={{ minWidth: '150px' }}>
-                <Typography variant="h6">Jyp Phyllis Galan</Typography>
-                <Typography variant="caption">jpcgalan@addu.edu.ph</Typography><br/>
-                <Typography variant="caption">09178710948</Typography>
-              </Grid>
-              <Grid item sm style={{ minWidth: '150px' }}>
-                <Typography variant="h6">Brian Joseph Unabia</Typography>
-                <Typography variant="caption">bjmunabia@addu.edu.ph</Typography><br/>
-                <Typography variant="caption">09084056623</Typography>
-              </Grid>
-            </Grid>
+          <Grid item md={5} xs={12}>
+            <iframe className={classes.avp} src="https://www.youtube.com/embed/Fq8qcSEBeV8?controls=1&loop=1&autoplay=1" frameborder="0" allow="accelerometer; autoplay; modestbranding; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </Grid>
+        </Grid>
+
+        <div style={{ height: 100 }}></div>
+
+        <Grid container direction="row" spacing={6} className={classes.contentContainer}>
+          <Grid item xs={12} sm={6} md={3}>
+            <Card className={classes.cardContainer}>
+              <CardActionArea onClick={() => handleClickOpen(0)}>
+                <CardMedia
+                  image={`${cdnURL}/Constitution_0.jpg`}
+                  title="Constitution"
+                  className={classes.imageContainer}
+                />
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Card className={classes.cardContainer}>
+              <CardActionArea onClick={() => handleClickOpen(1)}>
+                <CardMedia
+                  image={`${cdnURL}/Function_0.jpg`}
+                  title="Function"
+                  className={classes.imageContainer}
+                />
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Card className={classes.cardContainer}>
+              <CardActionArea onClick={() => handleClickOpen(2)}>
+                <CardMedia
+                  image={`${cdnURL}/Revise_0.jpg`}
+                  title="Revise"
+                  className={classes.imageContainer}
+                />
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Card className={classes.cardContainer}>
+              <CardActionArea onClick={() => handleClickOpen(3)}>
+                <CardMedia
+                  image={`${cdnURL}/Ratify_0.jpg`}
+                  title="Ratify"
+                  className={classes.imageContainer}
+                />
+              </CardActionArea>
+            </Card>
+          </Grid>
+        </Grid>
+
+        <div style={{ height: 100 }}></div>
+
+        <Grid container direction="row" spacing={6} className={classes.contentContainer}>
+          <Grid item xs={12} md={6}>
+            <img src={`${cdnURL}/Timeline.jpg`} width="100%" />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h4" component="h2" style={{ lineHeight: '1.5em' }}>
+              #YesToConsti
+            </Typography>
+            <Typography variant="body1" component="div" style={{ marginTop: '10px', marginBottom: '10px' }}>
+              <p>Ateneans, tuparin natin ang pangarap. Go out and vote <b>YES</b> during the plebiscite.</p>
+            </Typography>
+            <Typography variant="h4" component="h2" style={{ lineHeight: '1.5em', marginBottom: '10px' }}>
+              #DearCONCON
+            </Typography>
+            <Carousel autoPlay={false} showThumbs={true} showArrows={true}>
+              <div>
+                <img src={`${cdnURL}/DearConcon_0.jpg`} />
+              </div>
+              <div>
+                <img src={`${cdnURL}/DearConcon_1.jpg`} />
+              </div>
+              <div>
+                <img src={`${cdnURL}/DearConcon_2.jpg`} />
+              </div>
+            </Carousel>
           </Grid>
         </Grid>
       </div>
+
+      <Dialog
+        fullScreen={fullScreen}
+        open={openInfog}
+        onClose={handleClose}
+        aria-labelledby="responsive-dialog-title"
+      >
+        <DialogContent>
+          {slide}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
